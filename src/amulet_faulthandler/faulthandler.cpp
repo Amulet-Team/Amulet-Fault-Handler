c8a104c8cacaeb6f8cacf46b80505d1a44697a13
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-02-12 09:51:44 +0000

    Remove stack printing
    The stack was inaccurate making it useless

--- src/amulet_faulthandler/faulthandler.cpp
+++ src/amulet_faulthandler/faulthandler.cpp
@@ -6,81 +6,7 @@
 
 #include <dbghelp.h>
 
-#include <memory>
 #include <string>
-
-// https://stackoverflow.com/questions/22467604/how-can-you-use-capturestackbacktrace-to-capture-the-exception-stack-not-the-ca
-
-const int MaxNameLen = 256;
-
-void dump_stack(EXCEPTION_POINTERS* p)
-{
-    HANDLE process = GetCurrentProcess();
-    HANDLE thread = GetCurrentThread();
-    SymInitialize(process, NULL, TRUE);
-    CONTEXT ctx = *p->ContextRecord;
-    STACKFRAME64 frame {};
-
-#if defined(_M_AMD64)
-    frame.AddrPC.Offset = ctx.Rip;
-    frame.AddrPC.Mode = AddrModeFlat;
-    frame.AddrStack.Offset = ctx.Rsp;
-    frame.AddrStack.Mode = AddrModeFlat;
-    frame.AddrFrame.Offset = ctx.Rbp;
-    frame.AddrFrame.Mode = AddrModeFlat;
-#define MACHINE IMAGE_FILE_MACHINE_AMD64
-
-#elif defined(_M_IX86)
-    frame.AddrPC.Offset = ctx.Eip;
-    frame.AddrPC.Mode = AddrModeFlat;
-    frame.AddrStack.Offset = ctx.Esp;
-    frame.AddrStack.Mode = AddrModeFlat;
-    frame.AddrFrame.Offset = ctx.Ebp;
-    frame.AddrFrame.Mode = AddrModeFlat;
-#define MACHINE IMAGE_FILE_MACHINE_I386
-
-#else
-#error "Unsupported architecture"
-
-#endif
-
-    printf("*** Exception 0x%x occured ***\n", p->ExceptionRecord->ExceptionCode);
-
-    while (StackWalk64(
-        MACHINE, process, thread, &frame, &ctx, NULL, SymFunctionTableAccess64, SymGetModuleBase64, NULL)) {
-        // get symbol name for address
-        char symbol_buffer[sizeof(SYMBOL_INFO) + MAX_SYM_NAME * sizeof(TCHAR)];
-        SYMBOL_INFO* symbol = reinterpret_cast<SYMBOL_INFO*>(symbol_buffer);
-        symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
-        symbol->MaxNameLen = MAX_SYM_NAME;
-        DWORD64 displacement = 0;
-        if (!SymFromAddr(process, frame.AddrPC.Offset, &displacement, symbol)) {
-            strcpy_s(symbol->Name, MAX_SYM_NAME, "<unknown>");
-        }
-
-        IMAGEHLP_LINE64 line = { sizeof(IMAGEHLP_LINE64) };
-
-        // try to get line
-        DWORD disp;
-        if (SymGetLineFromAddr64(process, frame.AddrPC.Offset, &disp, &line)) {
-            printf("in %s\n\tat %s, line: %lu, address: 0x%llx\n", line.FileName, symbol->Name, line.LineNumber, symbol->Address);
-        } else {
-            // failed to get line
-            HMODULE module_handle = NULL;
-            char module_name[MaxNameLen] = {};
-            GetModuleHandleExA(
-                GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
-                reinterpret_cast<LPCTSTR>(frame.AddrPC.Offset),
-                &module_handle);
-
-            // at least print module name
-            if (module_handle != NULL)
-                GetModuleFileNameA(module_handle, module_name, MaxNameLen);
-
-            printf("in %s\n\tat %s, address 0x%llx.\n", module_name, symbol->Name, symbol->Address);
-        }
-    }
-}
 
 static std::wstring _path;
 static bool _full_dump = false;
@@ -117,7 +43,6 @@ void dump_process(EXCEPTION_POINTERS* p)
 LONG WINAPI CrashHandler(EXCEPTION_POINTERS* p)
 {
     dump_process(p);
-    dump_stack(p);
     return EXCEPTION_EXECUTE_HANDLER;
 }
 
