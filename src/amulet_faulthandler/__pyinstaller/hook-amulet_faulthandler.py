d553307a246d164ad0cfd7a4d5d7a61df3dfc813
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-03-16 16:45:29 +0000

    Improve pyinstaller import hook

--- src/amulet_faulthandler/__pyinstaller/hook-amulet_faulthandler.py
+++ src/amulet_faulthandler/__pyinstaller/hook-amulet_faulthandler.py
@@ -1,4 +1,3 @@
 from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 
 hiddenimports = collect_submodules("amulet_faulthandler")
-datas = collect_data_files("amulet_faulthandler")
