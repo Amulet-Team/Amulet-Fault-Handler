7182839c2a28e48ee522dd24c77bd3c2ac9e8d69
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-06-23 16:45:23 +0100

    Improve faulthandler
    Save crash log in a new thread.
    Switch to vectored exception handler to run before Python's exception handler.
    Add tests for stack overflow and heap corruption

--- src/amulet_faulthandler/faulthandler.cpp
+++ src/amulet_faulthandler/faulthandler.cpp
@@ -1,20 +1,55 @@
 #include "faulthandler.hpp"
 
-#if WIN32
+#if _WIN32
 
 #include <windows.h>
 
 #include <dbghelp.h>
 
+#include <mutex>
+#include <optional>
 #include <string>
 
-static std::wstring _path;
-static bool _full_dump = false;
+struct DumpConfig {
+    std::wstring path;
+    std::wstring ext;
+    bool full_dump;
+    std::uint16_t dump_index;
+};
 
-void dump_process(EXCEPTION_POINTERS* p)
-{
+static std::mutex _lock;
+static std::optional<DumpConfig> _dump_config;
+
+struct CrashStack {
+    DWORD code;
+    EXCEPTION_POINTERS* exc_info;
+    DWORD crashing_thread_id;
+    HANDLE done_event;
+    HANDLE thread;
+};
+
+thread_local static CrashStack _crash_stack;
+
+static DWORD WINAPI dump_thread(LPVOID param) {
+    std::lock_guard lock(_lock);
+    if (!_dump_config) {
+        return 0;
+    }
+
+    auto* args = static_cast<CrashStack*>(param);
+
+    // Create the path
+    wchar_t path[MAX_PATH + 100];
+    swprintf_s(
+        path,
+        L"%ls_%u%ls",
+        _dump_config->path.c_str(),
+        _dump_config->dump_index++,
+        _dump_config->ext.c_str());
+
+    // Open the file
     HANDLE dump_handle = CreateFileW(
-        _path.c_str(),
+        path,
         GENERIC_WRITE,
         0,
         nullptr,
@@ -23,36 +58,69 @@ void dump_process(EXCEPTION_POINTERS* p)
         nullptr);
 
     if (dump_handle != INVALID_HANDLE_VALUE) {
+        // Write the dump
         MINIDUMP_EXCEPTION_INFORMATION info;
-        info.ThreadId = GetCurrentThreadId();
-        info.ExceptionPointers = p;
+        info.ThreadId = args->crashing_thread_id;
+        info.ExceptionPointers = args->exc_info;
         info.ClientPointers = TRUE;
 
         MiniDumpWriteDump(
             GetCurrentProcess(),
             GetCurrentProcessId(),
             dump_handle,
-            _full_dump ? MiniDumpWithFullMemory : MiniDumpNormal,
+            _dump_config->full_dump ? MiniDumpWithFullMemory : MiniDumpNormal,
             &info,
             nullptr,
             nullptr);
+        // Close the file
         CloseHandle(dump_handle);
     }
+
+    SetEvent(args->done_event);
+    return 0;
 }
 
-LONG WINAPI CrashHandler(EXCEPTION_POINTERS* p)
+static LONG WINAPI exc_handler(EXCEPTION_POINTERS* exc_info) noexcept
 {
-    dump_process(p);
-    return EXCEPTION_EXECUTE_HANDLER;
+    // Get the crash code
+    _crash_stack.code = exc_info->ExceptionRecord->ExceptionCode;
+    // Skip some errors and non-errors
+    if (_crash_stack.code & 0x80000000 && _crash_stack.code != 0xE06D7363 && _crash_stack.code != 0xE0434352 && _crash_stack.code != STATUS_GUARD_PAGE_VIOLATION) {
+        _crash_stack.exc_info = exc_info;
+        _crash_stack.crashing_thread_id = GetCurrentThreadId();
+        _crash_stack.done_event = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+        _crash_stack.thread = CreateThread(nullptr, 0, dump_thread, &_crash_stack, 0, nullptr);
+        if (_crash_stack.thread) {
+            WaitForSingleObject(_crash_stack.done_event, 30000);
+            CloseHandle(_crash_stack.thread);
+        }
+        CloseHandle(_crash_stack.done_event);
+    }
+    // Continue to the next handler
+    return EXCEPTION_CONTINUE_SEARCH;
 }
 
 namespace Amulet {
 namespace faulthandler {
     void install(std::filesystem::path path, bool full_dump)
     {
-        _path = path.wstring();
-        _full_dump = full_dump;
-        SetUnhandledExceptionFilter(CrashHandler);
+        std::lock_guard lock(_lock);
+        if (_dump_config) {
+            throw std::runtime_error("Faulthandler is already installed");
+        }
+        _dump_config.emplace();
+        _dump_config->path = path.replace_extension().wstring();
+        _dump_config->ext = path.extension().wstring();
+        if (_dump_config->ext.empty()) {
+            _dump_config->ext = L".dmp";
+        }
+        if (MAX_PATH < _dump_config->path.size() + _dump_config->ext.size() + 6) {
+            _dump_config = std::nullopt;
+            throw std::runtime_error("Path is too long for Windows");
+        }
+        _dump_config->full_dump = full_dump;
+        _dump_config->dump_index = 0;
+        AddVectoredExceptionHandler(1, exc_handler);
     }
 }
 }
