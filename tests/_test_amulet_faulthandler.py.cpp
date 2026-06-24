d3cd0e1d2436f2c43036b633991a4ea64c3539f5
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-06-24 09:16:18 +0100

    Revert to double free behaviour

--- tests/_test_amulet_faulthandler.py.cpp
+++ tests/_test_amulet_faulthandler.py.cpp
@@ -1,9 +1,5 @@
 #include <pybind11/pybind11.h>
 
-#ifdef _WIN32
-#include <windows.h>
-#endif
-
 namespace py = pybind11;
 
 void throw_access_violation(){
@@ -15,20 +11,11 @@ void throw_access_violation(){
 void throw_stack_overflow(){
     throw_stack_overflow();
 }
-#pragma optimize("", on)
 
-#pragma optimize("", off)
 void throw_heap_corruption(){
-#ifdef _WIN32
-    HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, NULL, 0);
-#endif
-    char* cp = new char[32];
-    for (int i = 1; i <= 64; i++) {
-        *(cp - i) = 0xFF;
-        *(cp + 32 + i) = 0xFF;
-    }
-    delete[] cp;
-    delete[] cp;
+    int* p = new int;
+    delete p;
+    delete p;
 }
 #pragma optimize("", on)
 
